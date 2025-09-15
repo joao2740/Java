fb9c41e427de41b0df0bf6be7c21779777227b94
JOAO VITOR <149392154+joao2740@users.noreply.github.com>
2025-09-15 14:39:40 -0300

    Delete 03_Melhorando_Banco.sql

--- 03_Melhorando_Banco.sql
+++
@@ -1,9 +0,0 @@
-/* 6. Modificando e melhorando o banco*/
--- 6.1 Renomeando Colunas
-alter table clientes
-change column telefone celular varchar(15);
--- 6.2 Renomeando Tabela
-rename table clientes to usuarios;
--- alternando tipo de dados dee uma coluna 
-alter table usuarios 
-add column telefone varchar(12) after email;usuarios
